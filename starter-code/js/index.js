51681f3922524dbd500c3e1578fae2bfc06d47f2
Marcos Gomez <marcosgomezsanchez@gmail.com>
2019-10-28 14:15:58 +0000

    Ite 5: create product. Lab complete solution

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -32,14 +32,6 @@ const $calculateTrigger = document.getElementById("calculate");
 $calculateTrigger.addEventListener("click", calculateAll);
 
 // ITERATION 4
-window.addEventListener("load", function() {
-  var deleteButtons = document.getElementsByClassName("btn-remove");
-
-  //Loop to get all the delete buttons in our shop cart
-  for (var i = 0; i < deleteButtons.length; i++) {
-    deleteButtons[i].addEventListener("click", deleteProduct);
-  }
-});
 
 function deleteProduct(event) {
   // $event will give us through currentTarget the remove button we are clicking.
@@ -56,5 +48,53 @@ function deleteProduct(event) {
 // ITERATION 5
 
 function createProduct(event) {
-  // ...
+  const $createRow = event.currentTarget.parentNode.parentNode;
+
+  const $productNameInput = $createRow.querySelector('input[type="text"]');
+  const $productPriceInput = $createRow.querySelector('input[type="number"]');
+
+  const nameValue = $productNameInput.value;
+  const priceValue = $productPriceInput.valueAsNumber;
+
+  const $tableRow = document.createElement("tr");
+  $tableRow.classList.add("product");
+  $tableRow.innerHTML += `
+    <td class="name">
+      <span>${nameValue}</span>
+    </td>
+    <td class="price">$<span>${priceValue.toFixed(2)}</span></td>
+    <td class="quantity">
+      <input type="number" value="0" min="0" placeholder="Quantity" />
+    </td>
+    <td class="subtotal">$<span>0</span></td>
+    <td class="action">
+      <button class="btn btn-remove">Remove</button>
+    </td>
+  `;
+
+  const $removeButton = $tableRow.querySelector(".btn-remove");
+  console.log($removeButton);
+  $removeButton.addEventListener("click", deleteProduct);
+
+  const $parent = document.querySelector("#cart tbody");
+
+  $parent.appendChild($tableRow);
+
+  $productNameInput.value = "";
+  $productPriceInput.valueAsNumber = 0;
 }
+
+//Event Listener for Iteration 4 & 5
+window.addEventListener("load", function() {
+  var deleteButtons = document.getElementsByClassName("btn-remove");
+
+  //Loop to get all the delete buttons in our shop cart
+  for (var i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].addEventListener("click", deleteProduct);
+  }
+
+  const $createButton = document.getElementById("create");
+  if ($createButton) {
+    $createButton.addEventListener("click", createProduct);
+  }
+});
